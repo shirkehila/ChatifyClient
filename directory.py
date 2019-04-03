e500836abf9a696dc630d0507a4d67aa5bb836ad
Shir Kehila <shirkpro@gmail.com>
2019-04-04 00:45:35 +0300

    Remove print for debug

--- directory.py
+++ directory.py
@@ -17,25 +17,10 @@ class DirTree(tk.Frame):
         self.tree["columns"] = ("type")
         self.tree.column("type",width=50)
         self.tree.heading("type", text="Type")
-        #abspath = os.path.abspath(path)
-        # root_node = self.tree.insert('', 'end', text=abspath, open=True)
-        #self.process_directory('', abspath)
         self.tree.grid(row=0, column=0)
         ysb.grid(row=0, column=1, sticky='ns')
         xsb.grid(row=1, column=0, sticky='ew')
         self.grid()
-
-    def process_directory(self, parent, path):
-        for p in os.listdir(path):
-            abspath = os.path.join(path, p)
-            isdir = os.path.isdir(abspath)
-            if isdir:
-                oid = self.tree.insert(parent, 'end', text=p, open=False)
-                self.process_directory(oid, abspath)
-            else:
-                filename, file_extension = os.path.splitext(p)
-                oid = self.tree.insert(parent, 'end', text=filename, open=False, values=(file_extension[1:]))
-
 
     def process_xml(self, xml):
         root = ET.fromstring(xml)
@@ -57,16 +42,14 @@ class DirTree(tk.Frame):
     def get_selected_path(self):
         item_id = self.tree.focus()
         try:
-             f_type = self.tree.item(item_id)['values'][0]
+            f_type = self.tree.item(item_id)['values'][0]
         except IndexError:
             messagebox.showinfo("Error", "You tried to download a directory")
             return None
         return self.get_selected_rec(item_id) + '.' + f_type
 
     def get_selected_rec(self, item_id):
-        print(self.tree.item(item_id))
         name = self.tree.item(item_id)['text']
-        print(name)
         if self.tree.parent(item_id) == "":
             return name
         return os.path.join(self.get_selected_rec(self.tree.parent(item_id)), name)
